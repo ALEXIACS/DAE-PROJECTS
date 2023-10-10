7de82bfbc266a7327a058b1e8a7285daf2d0ad0d
ALEXIACS <147105627+ALEXIACS@users.noreply.github.com>
2023-10-10 17:59:02 -0400

    Add files via upload

---
+++ Py1.101.py
@@ -0,0 +1,21 @@
+while True: 
+  userName = input ("Hi! What is your name? ")
+  print ("Hi! " + userName)
+  answer = input("Would you like to study? (◍＞◡＜◍) (y/n)  ") .lower()
+
+  if answer == 'y':
+#simulating loading bar till added
+    print ("PREPARING MATERIALS...")
+    print ("Redirecting...")
+    print ("...")
+    print ("redirecting")
+    import webbrowser
+    url= "https://www.khanacademy.org/science/ms-biology"
+    webbrowser.open(url)
+    break
+  else:
+    print("You'll get bad grades ಠ╭╮ಠ ")
+    answer3 = input("Do you want to proceed? ¯\_(ツ)_/¯ ? (y/n) ") .lower()
+    if answer3 == 'y':
+       print (" (ノಠ益ಠ)ノ彡┻━┻   ... hehehe... You will not scape") 
+       line_number = 4
